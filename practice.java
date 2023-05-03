d3eff771759a45e70b352d7c4c2c5b5de5665356
EC2 Default User <radball0506@gmail.com>
2023-05-03 13:08:32 +0000

    java練習

---
+++ practice.java
@@ -0,0 +1,27 @@
+public class practice{
+  
+  public static void main( String[] args ){
+  //Hello World”と表示するプログラムを作成しなさい。
+    System.out.println("Hello World");
+    
+  //int 型の変数 x に数値 11 を代入し、x の値を“x=11”のように表示するプログラムを作成しなさい。
+    int x;
+
+    x = 11;
+    
+  //int 型の変数 a、b に数値 13、17 を代入し、a、b の値を“a=13,b=17”のように表示するプログラムを作成しなさい。
+    System.out.println( "x=" + x );
+    
+    int a;
+    int b;
+
+    a = 13;
+    b = 17;
+    
+    System.out.println( "a=" + a + ",b=" + b );
+
+  }
+}
+
+
+  
