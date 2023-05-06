d2a16c3c1f40a9fe90772065a103ba4c1d74a531
EC2 Default User <radball0506@gmail.com>
2023-05-06 12:29:10 +0000

    Java練習

--- practice.java
+++ practice.java
@@ -6,7 +6,7 @@ public class practice{
     
   //int 型の変数 x に数値 11 を代入し、x の値を“x=11”のように表示するプログラムを作成しなさい。
     int x;
-
+    
     x = 11;
     
   //int 型の変数 a、b に数値 13、17 を代入し、a、b の値を“a=13,b=17”のように表示するプログラムを作成しなさい。
@@ -14,11 +14,31 @@ public class practice{
     
     int a;
     int b;
-
+    
     a = 13;
     b = 17;
     
     System.out.println( "a=" + a + ",b=" + b );
+    
+  //int 型の変数 c に数値 13 と 17 の和を代入し、c の値を表示するプログラムを作成しなさい。
+    int c;
+    
+    c = 13 + 17;
+    System.out.println( "c=" + c );
+    
+  //数値 13 と 17 の積を表示するプログラムを作成しなさい。ただし、変数を使用しないこと。
+    System.out.println( 13 * 17 );
+    
+  //次のプログラムを作成しなさい。int 型の変数 d に数値 7 を代入する。変数 d の値を 3 倍にする。
+  //変数 d の値を表示する。変数 d の値を半分（1/2）にする。変数 d の値を表示する。
+    int d;
+    
+    d = 7;
+    d *= 3;
+    System.out.println( "d=" + d );
+    d /= 2;
+    System.out.println( "d=" + d );
+    
 
   }
 }
